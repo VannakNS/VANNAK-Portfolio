0398be04e1c4c9b33aebcfd89d67e9866796d9fd
vannakv405@gmail.com <vannakv405@gmail.com>
2024-08-13 22:52:51 +0700

    Resol

---
+++ front-end/webpack.config.js
@@ -0,0 +1,11 @@
+// webpack.config.js
+module.exports = {
+  // other configurations...
+  output: {
+    publicPath: "/",
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+};
+// webpack.config.js
